28f290a3d45abaa8deab6e7c7ad0040e2ee1d208
Aniket Kamble <aktitanium8055@gmail.com>
2024-12-17 20:55:15 +0530

    Update dsprac3.py

--- dsprac3.py
+++ dsprac3.py
@@ -1,3 +1,11 @@
+"""
+Write a Python program for department library which has N books, write functions for following: 
+a) Delete the duplicate entries 
+b) Display books in ascending order based on cost of books 
+c) Count number of books with cost more than 500. 
+d) Copy books in a new list which has cost less than 500. 
+"""
+
 class Book:
     def __init__(self, title, author, cost):
         self.title = title
