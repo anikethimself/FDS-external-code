67c832a9059bb6f148a45121a9c379f156aed89c
Aniket Kamble <aktitanium8055@gmail.com>
2024-12-18 16:55:48 +0530

    Update dsprac4.py

--- dsprac4.py
+++ dsprac4.py
@@ -1,28 +1,24 @@
-"""
-Write a python program to store roll numbers of student in array who attended training program in random order. 
-Write function for searching whether particular student attended training program or not, using Linear search.
-"""
-
 def linear_search(arr, target):
     for i in range(len(arr)):
-        if arr[i] == target:
-            return i  
-    return -1  
+        if arr[i]==target:
+            return i 
+    return -1
 
 def main():
-    roll_numbers = [101, 102, 103, 104, 105, 106, 107]
-
-    import random
-    random.shuffle(roll_numbers)
-
-    print("Roll numbers of students who attended the training program:", roll_numbers)
-    target_roll_number = int(input("Enter the roll number to search: "))
+    roll_no=[101,102,103,104,105,106,107]
     
-    index_linear = linear_search(roll_numbers, target_roll_number)
-    if index_linear != -1:
-        print(f"Linear Search: Roll number {target_roll_number} found at index {index_linear}.")
+    import random
+    random.shuffle(roll_no)
+    print("Roll no. of students who attended the training program:",roll_no)
+    
+    target_roll=int(input("Enter the roll no to search:"))
+    
+    index=linear_search(roll_no,target_roll)
+    
+    if index!=-1:
+        print(f"The roll no {target_roll} is found at index{index}")
     else:
-        print(f"Linear Search: Roll number {target_roll_number} not found.")
-
-if __name__ == "__main__":
+        print("the roll no is not found")
+    
+if __name__=="__main__":
     main()
